c4af77c0fab7352ad23cdf393559b0b85258f84c
liyuelin.kyssion <liyuelin.kyssion@bytedance.com>
2023-09-21 11:49:36 +0800

    feat: add case

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -8,7 +8,7 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setWindowedMode(2000,1000);
+		config.setWindowedMode(1000,500);
 		config.setForegroundFPS(60);
 		config.setTitle("My GDX Game");
 //		new Lwjgl3Application(new findBug(), config);
